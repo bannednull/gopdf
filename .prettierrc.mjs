8bfde022129841a54422ec0ab319b643d3789fb1
bannednull <bannednull@gmail.com>
2025-03-16 14:22:46 -0300

    prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,23 @@
+/** @type {import("prettier").Config} */
+
+export default {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  plugins: [
+    'prettier-plugin-astro',
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
